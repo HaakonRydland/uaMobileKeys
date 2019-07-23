558fd5c2b36cbafb4235d6412f187acbeb10e46b
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-07-23 08:47:41 +0200

    Lagt til kommentarer og implementert isEndpointSetup

--- src/android/UaMobileKeysApi.java
+++ src/android/UaMobileKeysApi.java
@@ -13,36 +13,70 @@ import com.assaabloy.mobilekeys.api.MobileKeysApi;
 import com.assaabloy.mobilekeys.api.ReaderConnectionController;
 import com.assaabloy.mobilekeys.api.ble.*;
 import com.assaabloy.mobilekeys.api.hce.NfcConfiguration;
+import com.assaabloy.mobilekeys.api.MobileKeysCallback;
+import com.assaabloy.mobilekeys.api.MobileKeysException;
 
-public class UaMobileKeysApi extends CordovaPlugin {
+public class UaMobileKeysApi extends CordovaPlugin implements MobileKeysCallback {
 
     public UaMobileKeysApi () {}
-
     private MobileKeysApiFacade mobileKeysApiFacade;
 
     // Mobile Keys interface
+    // applicationStartup
     public void startup(CallbackContext callbackContext) {
+        //MobileKeys mobileKeys = mobileKeysApiFacade.getMobileKeys();
+        //mobileKeys.applicationStartup(this);
+
         callbackContext.success("Reached startup()");
     }
 
+    // isEndpointSetupComplete
     public void isEndpointSetup(CallbackContext callbackContext) {
-        callbackContext.success("Reached isEndpointSetup()");
+        MobileKeys mobileKeys = mobileKeysApiFacade.getMobileKeys();
+        boolean isEndpointSetup = false;
+        try
+        {
+            isEndpointSetup = mobileKeys.isEndpointSetupComplete();
+        }
+        catch (MobileKeysException e)
+        {
+            callbackContext.error("Something went wrong in isEndpointSetup()");
+        }
+
+        callbackContext.success(isEndpointSetup);
     }
 
+    // endpointSetup
     public void setupEndpoint(CallbackContext callbackContext) {
         callbackContext.success("Reached setupEndpoint()");
     }
 
+    // endpointUpdate
     public void updateEndpoint(CallbackContext callbackContext) {
         callbackContext.success("Reached updateEndpoint()");
     }
 
+    // listMobileKeys
     public void listMobileKeys(CallbackContext callbackContext) {
         callbackContext.success("Reached listMobileKeys()");
     }
 
+    // getEnpointInfo
     public void endpointInfo(CallbackContext callbackContext) {
         callbackContext.success("Reached endpointInfo()");
+    }
+
+    // Interface implementations
+    @Override
+    public void handleMobileKeysTransactionCompleted()
+    {
+        mobileKeysApiFacade.onStartUpComplete();
+    }
+
+    @Override
+    public void handleMobileKeysTransactionFailed(MobileKeysException mobileKeysException)
+    {
+
     }
 
 }
