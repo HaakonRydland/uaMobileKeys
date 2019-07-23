46a7d8f13844b314230847294cb24c14a5a1e178
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-07-23 13:04:55 +0200

    Cordovaendring

--- src/android/UaMobileKeysSetup.java
+++ src/android/UaMobileKeysSetup.java
@@ -16,7 +16,7 @@ public class UaMobileKeysSetup extends CordovaPlugin {
 
     int androidVersionCurrentlyRunning = Build.VERSION.SDK_INT;
     //Context context = this.cordova.getActivity().getApplicationContext();
-    Context context = (androidVersionCurrentlyRunning >= 21) ? this.cordova.getActivity().getWindow().getContext() : this.cordova.getActivity().getApplicationContext();
+    Context context = (androidVersionCurrentlyRunning >= 21) ? this.Cordova.getActivity().getWindow().getContext() : this.Cordova.getActivity().getApplicationContext();
 
     public void initializeMobileKeysApi()
     {
