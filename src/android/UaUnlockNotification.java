a4ae26a4d0de38c15b57d0990a857ab8261387c6
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-07-26 14:19:55 +0200

    Nærmer seg

--- src/android/UaUnlockNotification.java
+++ src/android/UaUnlockNotification.java
@@ -6,6 +6,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
+import android.support.v4.app;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.BigTextStyle;
 import android.support.v4.content.ContextCompat;
