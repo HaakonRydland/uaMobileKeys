c44040129b879359cd55c7bb9dfd4fe7fb53c34c
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-07-26 08:37:04 +0200

    Import statement

--- src/android/UaMobileKeysApi.java
+++ src/android/UaMobileKeysApi.java
@@ -2,6 +2,7 @@ package cordova.plugin.uamobilekeys;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
