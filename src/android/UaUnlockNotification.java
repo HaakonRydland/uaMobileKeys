9e62908e0218fd19528bef7ac70844071eca8a10
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-08-20 15:00:43 +0200

    Prøver android ikon

--- src/android/UaUnlockNotification.java
+++ src/android/UaUnlockNotification.java
@@ -55,11 +55,11 @@ public final class UaUnlockNotification
 
     public static NotificationCompat.Builder notificationBuilder(Context context, String channelId)
     {
-        Resources r = context.getResources();
-        int drawableId = r.getIdentifier("ic_stat_vpn_key", "drawable", "cordova.plugin.uamobilekeys");
+        //Resources r = context.getResources();
+        //int drawableId = r.getIdentifier("ic_stat_vpn_key", "drawable", "cordova.plugin.uamobilekeys");
 
         return new NotificationCompat.Builder(context, channelId)
                 //.setColor(ContextCompat.getColor(context))
-                .setSmallIcon(drawableId);
+                .setSmallIcon(android.R.drawable.ic_dialog_info);
     }
 }
