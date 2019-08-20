a1cb3b1bbe9562577f8bfa9eeab9a913da210e88
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-08-20 12:50:27 +0200

    hheehe

--- src/android/UaUnlockNotification.java
+++ src/android/UaUnlockNotification.java
@@ -56,7 +56,7 @@ public final class UaUnlockNotification
     public static NotificationCompat.Builder notificationBuilder(Context context, String channelId)
     {
         Resources r = getResources();
-        int drawableId = r.getIdentifier("ic_stat_vpn_key", "drawable", "cordova.plugin.uamobilekeys;")
+        int drawableId = r.getIdentifier("ic_stat_vpn_key", "drawable", "cordova.plugin.uamobilekeys");
 
         return new NotificationCompat.Builder(context, channelId)
                 //.setColor(ContextCompat.getColor(context))
