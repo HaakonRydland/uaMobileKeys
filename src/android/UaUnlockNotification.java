d889931a6227d370a813d960162b1229f5caaf51
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-08-20 13:16:21 +0200

    Prøver med context

--- src/android/UaUnlockNotification.java
+++ src/android/UaUnlockNotification.java
@@ -55,7 +55,7 @@ public final class UaUnlockNotification
 
     public static NotificationCompat.Builder notificationBuilder(Context context, String channelId)
     {
-        Resources r = getResources();
+        Resources r = context.getResources();
         int drawableId = r.getIdentifier("ic_stat_vpn_key", "drawable", "cordova.plugin.uamobilekeys");
 
         return new NotificationCompat.Builder(context, channelId)
