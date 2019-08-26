254982ae17ec9099c8fad30895b3d6cc8b07d977
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-08-26 09:38:24 +0200

    Prøver å legge til location permissions

--- src/android/UaMobileKeysApi.java
+++ src/android/UaMobileKeysApi.java
@@ -12,6 +12,8 @@ import android.content.pm.PackageManager;
 import android.Manifest;
 import android.app.Notification;
 import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -33,6 +35,7 @@ public class UaMobileKeysApi extends CordovaPlugin implements MobileKeysCallback
     public UaMobileKeysApi () {}
     private MobileKeysApiFacade mobileKeysApiFacade;
     private UaMobileKeysSetup keySetup = new UaMobileKeysSetup();
+    private Snackbar mSnackbar;
 
     // Mobile Keys interface
     // applicationStartup
@@ -140,13 +143,17 @@ public class UaMobileKeysApi extends CordovaPlugin implements MobileKeysCallback
     // må implementere UaUnlockNotification før dette virker
     public void startForegroundScanning(CallbackContext callbackContext, Context context) {
         // check if app has locationPermissions - implement method
-        ReaderConnectionController controller = MobileKeysApi.getInstance().getReaderConnectionController();
-        controller.enableHce();
+        if (hasLocationPermissions()) {
+            ReaderConnectionController controller = MobileKeysApi.getInstance().getReaderConnectionController();
+            controller.enableHce();
 
-        Notification notification = UaUnlockNotification.create(context);
-        controller.startForegroundScanning(notification);
+            Notification notification = UaUnlockNotification.create(context);
+            controller.startForegroundScanning(notification);
 
-        callbackContext.success("Reached the end of startForegroundScanning");
+            callbackContext.success("Reached the end of startForegroundScanning");
+        } else {
+            requestLocationPermissions();
+        }
     }
 
     public void stopScanning(CallbackContext callbackContext) {
@@ -188,13 +195,28 @@ public class UaMobileKeysApi extends CordovaPlugin implements MobileKeysCallback
         }
     }
 
-    /*
     private boolean hasLocationPermissions()
     {
         return (ContextCompat.checkSelfPermission(requireContext(),
                 Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(requireContext(),
                         Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);
-    }*/
+    }
+
+    private void requestLocationPermissions() {
+        if (!hasLocationPermissions()) {
+            Snackbar.make(containerView,
+                    "Location services is required to open lock",
+                    Snackbar.LENGTH_INDEFINITE).setAction("allow", new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    KeysFragment.this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                            REQUEST_LOCATION_PERMISSION);
+                }
+            }).show();
+        }
+    }
 }
 
