a20e3323aab8cfc9d849be64005e25634705e76e
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2020-04-17 15:11:08 +0200

    Oppdatert struktur og benytter callback

--- src/ios/UaMobileKeys.m
+++ src/ios/UaMobileKeys.m
@@ -6,8 +6,9 @@
 #import <Cordova/CDV.h>
 
 @interface UaMobileKeys : CDVPlugin {
-  // Member variables go here.
+
 }
+
 @property(nonatomic) MobileKeysManager *mobileKeysManager;
 - (void)coolMethod:(CDVInvokedUrlCommand*)command;
 - (void)startup:(CDVInvokedUrlCommand*)command;
@@ -28,6 +29,13 @@
   NSArray<MobileKeysKey*> *_mobilekey;
   NSArray * _openingTypes;
 
+  // CallbackIds
+  NSString* _startupCallbackId;
+  NSString* _setupEndpointCallbackId;
+  NSString* _terminateEndpointCallbackId;
+  NSString* _updateEndpointCallbackId;
+
+#pragma mark - SetupMethods
 - (id)init:(CDVInvokedUrlCommand*)command {
     self = [super init];
 
@@ -36,69 +44,56 @@
         _openingTypes =@[@(MobileKeysOpeningTypeEnhancedTap)];
     }
     
-    CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"True"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    
     return self;
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+- (MobileKeysManager *)createInitializedMobileKeysManager {
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
+    NSString* applicationId = @"UaMobileKeys";
+    NSString *version = [NSString stringWithFormat:@"%@-%@ (%@)", applicationId, [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"], [[NSBundle mainBundle] infoDictionary][@"CFBundleVersion"]];
+    NSDictionary *config = @{MobileKeysOptionApplicationId: applicationId, MobileKeysOptionVersion: version};
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    return [[MobileKeysManager alloc] initWithDelegate:self options:config];
 }
 
-// Mobile keys core methods
+#pragma mark - SDK Methods
 - (void)startup:(CDVInvokedUrlCommand*)command
 {
+    _startupCallbackId = command.callbackId;
+
     if (self) {
         _mobileKeysManager = [self createInitializedMobileKeysManager];
         _openingTypes =@[@(MobileKeysOpeningTypeEnhancedTap)];
     }
 
-    CDVPluginResult* pluginResult = nil;
-
     [_mobileKeysManager startup];
-
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"True"];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)isEndpointSetup:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-
+    _setupEndpointCallbackId = command.callbackId;
     NSError *error;
     BOOL setupComplete = [_mobileKeysManager isEndpointSetup:&error];
     NSString *setupCompleteString = setupComplete ? @"True" : @"False";
 
-    [self handleError:error];
-
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:setupCompleteString];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    if (error != nil) {
+        [self handleEndpointSetupError:error];
+    } else {
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:setupCompleteString];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
 }
 
 - (void)setupEndpoint:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-
-    // NSString* results = @"True";
+    _setupEndpointCallbackId = command.callbackId;
     NSString* invitationCode = [command.arguments objectAtIndex:0];
 
+    CDVPluginResult* pluginResult = nil;
     if ([self isValidCode:invitationCode]) {
         [_mobileKeysManager setupEndpoint:invitationCode];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:invitationCode];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"True"];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Invalid code"];
     }
@@ -108,13 +103,9 @@
 
 - (void)updateEndpoint:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
+    _updateEndpointCallbackId = command.callbackId;
 
     [_mobileKeysManager updateEndpoint];
-
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"True"];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)listMobileKeys:(CDVInvokedUrlCommand*)command
@@ -137,12 +128,9 @@
 
 - (void)unregisterEndpoint:(CDVInvokedUrlCommand*)command
 {    
-    CDVPluginResult* pluginResult = nil;
-    [_mobileKeysManager unregisterEndpoint];
+    _terminateEndpointCallbackId = command.callbackId;
     
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"True"];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [_mobileKeysManager unregisterEndpoint];
 }
 
 - (void)startForegroundScanning:(CDVInvokedUrlCommand*)command
@@ -174,69 +162,133 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-// Error handeling and callbacks
-- (void)handleError:(NSError *)error {
-    if (error != nil) {
-        CDVPluginResult* pluginResult = nil;
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:error.localizedFailureReason];
-    }
-}
-
-- (void)mobileKeysDidStartup {
-    if (_applicationIsStarting) {
-        // [self setupEndpoint];
-        _applicationIsStarting = NO;
-    }
-}
-
-- (void)mobileKeysDidFailToStartup:(NSError *)error {
-    [self handleError:error];
-}
-
 - (BOOL) isEndpointSetup {
     NSError *error;
     BOOL setupComplete = [_mobileKeysManager isEndpointSetup:&error];
-    [self handleError:error];
 
     return setupComplete;
 }
 
-- (void) mobileKeysDidSetupEndpoint {
-    CDVPluginResult* pluginResult = nil;
-
-    if (!self.isEndpointSetup) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Endpoint is not setup"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:@"Callback"];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Endpoint is setup"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:@"Callback"];
+#pragma mark - Callback methods
+- (void)mobileKeysDidStartup {
+    NSString* callbackId = _startupCallbackId;
+    if (callbackId.length == 0) {
+        callbackId = @"Found no callbackId";
     }
+
+    if (_applicationIsStarting) {
+        _applicationIsStarting = NO;
+    }
+
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"True"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
 }
 
-- (void) mobileKeysDidFailToSetupEndpoint:(NSError * ) error {
-    [self handleError:error];
+- (void)mobileKeysDidFailToStartup:(NSError *)error {
+    [self handleStartupError:error];
+}
+
+- (void) mobileKeysDidSetupEndpoint {
+    NSString* callbackId = _setupEndpointCallbackId;
+    if (callbackId.length == 0) {
+        callbackId = @"Found no callbackId";
+    }
 
     CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Failed to setup endpoint"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:@"Callback"];
+    if (!self.isEndpointSetup) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Endpoint is not setup"];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Endpoint is setup"];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+}
+
+- (void)mobileKeysDidFailToSetupEndpoint:(NSError * ) error {
+    [self handleEndpointSetupError:error];
+}
+
+- (void)mobileKeysDidUpdateEndpoint {
+    NSString* callbackId = _updateEndpointCallbackId;
+    if (callbackId.length == 0) {
+        callbackId = @"Found no callbackId";
+    }
+
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"True"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
 }
 
 - (void)mobileKeysDidFailToUpdateEndpoint:(NSError *)error {
-    [self handleError:error];
-
-    CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Failed to update endpoint"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:@"Callback"];
+    [self handleUpdateEndpointError:error];
 }
 
 - (void)mobileKeysDidTerminateEndpoint {
+    NSString* callbackId = _terminateEndpointCallbackId;
+    if (callbackId.length == 0) {
+        callbackId = @"Found no callbackId";
+    }
+
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Failed to unregister endpoint"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:@"Callback"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
 }
 
-// Helper methods
+#pragma mark - Error handling
+- (void)handleStartupError:(NSError *) error {
+    if (error != nil) {
+        NSString* callbackId = _startupCallbackId;
+        if (callbackId.length == 0) {
+            callbackId = @"Found no callbackId";
+        }
+
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }
+}
+
+- (void)handleEndpointSetupError:(NSError *) error {
+    if (error != nil) {
+        NSString* callbackId = _setupEndpointCallbackId;
+        if (callbackId.length == 0) {
+            callbackId = @"Found no callbackId";
+        }
+
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }
+}
+
+- (void)handleUnregisterEndpointError:(NSError *) error {
+    if (error != nil) {
+        NSString* callbackId = _terminateEndpointCallbackId;
+        if (callbackId.length == 0) {
+            callbackId = @"Found no callbackId";
+        }
+
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }
+}
+
+- (void)handleUpdateEndpointError:(NSError *) error {
+    if (error != nil) {
+        NSString* callbackId = _updateEndpointCallbackId;
+        if (callbackId.length == 0) {
+            callbackId = @"Found no callbackId";
+        }
+
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+    }
+}
+
+#pragma mark - Helper methods
 - (BOOL)isValidCode:(NSString *)code {
     NSString *validPattern = @"^[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}$";
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:validPattern options:0 error:nil];
@@ -244,18 +296,19 @@
     return (code != nil) && ([regex firstMatchInString:code options:0 range:NSMakeRange(0, code.length)] != nil);
 }
 
-- (MobileKeysManager *)createInitializedMobileKeysManager {
+#pragma mark - Test methods
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
 
-    NSString* applicationId = @"UaMobileKeys";
-    NSString *version = [NSString stringWithFormat:@"%@-%@ (%@)", applicationId, [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"], [[NSBundle mainBundle] infoDictionary][@"CFBundleVersion"]];
-    NSDictionary *config = @{MobileKeysOptionApplicationId: applicationId, MobileKeysOptionVersion: version};
-    
-//     // Specify your own iBeacon UUID or use the internal one by not specifying this option
-//     // ***********************************************************************************
-//     // NSString *myBeaconUUIDString = @"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
-//     // NSDictionary *config = @{MobileKeysOptionApplicationId: APPLICATION_ID, MobileKeysOptionVersion: version, MobileKeysOptionBeaconUUID: myBeaconUUIDString};
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
 
-    return [[MobileKeysManager alloc] initWithDelegate:self options:config];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
